9aac9ca5415b364c5d29dfaaf354392aa0285311
Keefe Huang <keefe.huang@tum.de>
2024-02-21 16:23:37 +0100

    Implement `heat` and `random` variants for pivot qubit selection
    `heat` implements a "temperature" based weighted selection with probability ~ exp(-cost).
    `random` implements random selection of pivot qubit.

---
+++ experiments/choice_fn.py
@@ -0,0 +1,39 @@
+import math
+import random
+
+
+def heat_chooser(*args, **kwargs):
+    if not args:
+        raise TypeError('min expected 1 arguments, got 0')
+    elif len(args) == 1:
+        iterable = iter(args[0])
+    else:
+        iterable = args
+    key = None
+    for k, v in kwargs.items():
+        if k != 'key':
+            raise TypeError('min() got an unexpected keyword argument')
+        elif not callable(v):
+            raise TypeError(str(type(v).__name__) + ' object is not callable')
+        key = v
+    iterable = list(iterable)
+    weights = list(map(lambda x: math.exp(-x), map(key, iterable)))
+    weights = [float(i)/sum(weights) for i in weights]
+    return random.choices(iterable, weights=weights, k=1)[0]
+
+
+def random_chooser(*args, **kwargs):
+    if not args:
+        raise TypeError('min expected 1 arguments, got 0')
+    elif len(args) == 1:
+        iterable = iter(args[0])
+    else:
+        iterable = args
+    iterable = list(iterable)
+    return random.choice(iterable)
+
+
+if __name__ == '__main__':
+    weights = [(1, 1, 20), (2, 2, 4), (3, 3, 5)]
+    print(random_chooser(weights, key=lambda x: x[2]))
+    print(heat_chooser(weights, key=lambda x: x[2]))
