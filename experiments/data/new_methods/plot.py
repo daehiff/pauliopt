9a8413ea1897bf6b7a0610c5e7bc3533a16aa6e1
Keefe Huang <keefe.huang@tum.de>
2024-03-16 18:09:24 +0100

    Add latest code to generate legends and uniform naming for methods

--- experiments/data/new_methods/plot.py
+++ experiments/data/new_methods/plot.py
@@ -15,11 +15,112 @@ def plot_experiment(name="random_guadalupe", v_line_cx=None, v_line_depth=None):
 
     df = pd.read_csv(f"./{name}.csv")
     # df = df[df["method"] != "Ewout van den Berg (stim)"]
-    df['method'] = df['method'].replace({'qiskit transpile': 'qiskit',
+    df['method'] = df['method'].replace({'qiskit transpile': 'Transpile (Qiskit impl.)',
+                                         'ours': 'tableau (proposed)',
+                                         'ours_temp': 'tableau w/ heat (proposed)',
+                                         'ours_random': 'tableau w/ random pivot (proposed)',
+                                         'Bravyi et al. (qiskit)': 'Bravyi and Maslov (Qiskit impl.)',
+                                         'Ewout van den Berg (stim)': 'van den Berg (Stim impl.)',
+                                         'Maslov et al' : 'Maslov and Yang (Qiskit impl.)',
+                                         'Duncan et al (pyzx)': 'Duncan et al (pyzx impl.)'
+                                         })
+    df = df[df["method"] != "tableau w/ heat (proposed)"]
+    df = df[df["method"] != "tableau w/ random pivot (proposed)"]
+    fig = sns.lineplot(df, x="n_gadgets", y="h", hue="method")
+    handles, labels = fig.get_legend_handles_labels()
+    lgd = fig.legend(handles, labels, ncol=3, borderaxespad=1.)
+    lgd = fig.legend(handles, labels,
+                     bbox_to_anchor=(0.95, 1.35), ncol=3, borderaxespad=1.)
+    export_legend(lgd)
+    # fig.get_legend().remove()
+    # plt.title("H-Gates")
+    # plt.xlabel("Number of input Gates")
+    # plt.ylabel("Number of H-Gates")
+    # plt.tight_layout(pad=1.0)
+    # plt.savefig(f"./{name}_h.pdf")
+    # plt.show()
+    # plt.clf()
+
+    # fig = sns.lineplot(df, x="n_gadgets", y="s", hue="method")
+    # fig.get_legend().remove()
+    # plt.title("S-Gates")
+    # plt.xlabel("Number of input gates")
+    # plt.ylabel("Number of S-Gates")
+    # plt.tight_layout(pad=1.0)
+    # plt.savefig(f"./{name}_s.pdf")
+    # plt.show()
+    # plt.clf()
+
+    # fig = sns.lineplot(df, x="n_gadgets", y="cx", hue="method")
+    # fig.get_legend().remove()
+    # plt.title("CNOT-Gates")
+    # plt.xlabel("Number of input Gates")
+    # plt.ylabel("Number of CNOT-Gates")
+    # # add a vertical line for v_line_cx if not none
+    # if v_line_cx is not None:
+    #     plt.axhline(y=v_line_cx, color="black", linestyle="--")
+    # plt.tight_layout(pad=1.0)
+    # plt.savefig(f".//{name}_cx.pdf")
+    # plt.show()
+    # plt.clf()
+
+    # fig = sns.lineplot(df, x="n_gadgets", y="depth", hue="method")
+    # fig.get_legend().remove()
+    # plt.title("Circuit depth")
+    # plt.xlabel("Number of input Gates")
+    # plt.ylabel("Circuit Depth")
+    # # add a vertical line for v_line_cx if not none
+    # # if v_line_cx is not None:
+    # #     plt.axhline(y=v_line_cx, color="black", linestyle="--")
+    # plt.tight_layout(pad=1.0)
+    # plt.savefig(f".//{name}_depth.pdf")
+    # plt.show()
+    # plt.clf()
+
+    # fig = sns.lineplot(df, x="n_gadgets", y="2q_depth", hue="method")
+    # fig.get_legend().remove()
+    # plt.title("2 Qubit Gate depth")
+    # plt.xlabel("Number of input Gates")
+    # plt.ylabel("2 Qubit Gate Depth")
+    # # add a vertical line for v_line_cx if not none
+    # if v_line_depth is not None:
+    #     plt.axhline(y=v_line_depth, color="black", linestyle="--")
+    # plt.tight_layout(pad=1.0)
+    # plt.savefig(f".//{name}_2qdepth.pdf")
+    # plt.show()
+    # plt.clf()
+
+    # fig = sns.lineplot(df, x="n_gadgets", y="depth", hue="method")
+    # fig.get_legend().remove()
+    # plt.title("Circuit depth")
+    # plt.xlabel("Number of input Gates")
+    # plt.ylabel("Circuit Depth")
+    # # add a vertical line for v_line_cx if not none
+    # # if v_line_depth is not None:
+    # #     plt.axhline(y=v_line_depth, color="black", linestyle="--")
+    # plt.tight_layout(pad=1.0)
+    # plt.savefig(f".//{name}_depth.pdf")
+    # plt.show()
+    plt.clf()
+
+
+def plot_full_experiment(data="cx", v_line_cx=None, v_line_depth=None):
+    plt.rcParams.update({
+        "text.usetex": True,
+        "font.family": "sans-serif",
+        "font.size": 11
+    })
+    sns.set_palette(sns.color_palette("colorblind"))
+
+    df = pd.read_csv(f"./{name}.csv")
+    # df = df[df["method"] != "Ewout van den Berg (stim)"]
+    df['method'] = df['method'].replace({'qiskit transpile': 'Transpile (Qiskit impl.)',
                                          'ours': 'tableau (ours)',
                                          'ours_temp': 'tableau w/ heat (ours)',
                                          'ours_random': 'tableau w/ random pivot (ours)',
-                                         'Bravyi et al. (qiskit)': 'qiskit_tableau'})
+                                         'Bravyi et al. (qiskit)': 'Bravyi et al. (Qiskit impl.)',
+                                         'Ewout van den Berg (stim)': 'Van den Berg (Stim impl.)'
+                                         })
     df = df[df["method"] != "tableau w/ heat (ours)"]
     df = df[df["method"] != "tableau w/ random pivot (ours)"]
     sns.lineplot(df, x="n_gadgets", y="h", hue="method")
@@ -70,49 +171,181 @@ def plot_experiment(name="random_guadalupe", v_line_cx=None, v_line_depth=None):
     plt.savefig(f".//{name}_2qdepth.pdf")
     plt.show()
     plt.clf()
-# def plot_experiment(name="random_guadalupe", v_line_cx=None):
+
+    sns.lineplot(df, x="n_gadgets", y="depth", hue="method")
+    plt.title("Circuit depth")
+    plt.xlabel("Number of input Gates")
+    plt.ylabel("Circuit Depth")
+    # add a vertical line for v_line_cx if not none
+    # if v_line_depth is not None:
+    #     plt.axhline(y=v_line_depth, color="black", linestyle="--")
+    plt.savefig(f".//{name}_depth.pdf")
+    plt.show()
+    plt.clf()
+
+
+def export_legend(legend, filename="legend.pdf", expand=[-5, -5, 5, 5]):
+    fig = legend.figure
+    fig.canvas.draw()
+    bbox = legend.get_window_extent()
+    bbox = bbox.from_extents(*(bbox.extents + np.array(expand)))
+    bbox = bbox.transformed(fig.dpi_scale_trans.inverted())
+    fig.savefig(filename, dpi="figure", bbox_inches=bbox)
+
+
+# def plot_two_experiment(name_restricted="random_guadalupe", name_complete="random_complete_16", v_line_cx=None, v_line_depth=None):
 #     plt.rcParams.update({
 #         "text.usetex": True,
 #         "font.family": "sans-serif",
 #         "font.size": 11
 #     })
 #     sns.set_palette(sns.color_palette("colorblind"))
+#     name = name_restricted + '_' + name_complete
 
-#     df = pd.read_csv(f"./{name}.csv")
+#     df_restricted = pd.read_csv(f"./{name_restricted}.csv")
+#     df_complete = pd.read_csv(f"./{name_complete}.csv")
 #     # df = df[df["method"] != "Ewout van den Berg (stim)"]
-#     # df = df.groupby('N_gadgets').groupBy('method').agg(
-#     #     func=statistics.mean, axis=['H', 'S', 'CX', 'depth'])
-#     print(df.keys)
-#     df = pd.pivot_table(df, index=['n_gadgets', 'method'], values=[
-#                         'h', 's', 'cx', 'depth'], aggfunc='mean').reset_index()
-#     print(df.keys)
-#     df['method'] = df['method'].replace({'qiskit transpile': 'qiskit',
-#                                          'ours': 'tableau (ours)',
-#                                          'Bravyi et al. (qiskit)': 'qiskit_tableau'})
-#     print(df.keys)
-#     sns.lineplot(df, x="n_gadgets", y="h", hue="method")
-#     plt.title("H-Gates")
-#     plt.xlabel("Number of input Gates")
-#     plt.ylabel("Number of H-Gates")
-#     plt.savefig(f"./{name}_h.pdf")
-#     plt.show()
+#     df_restricted['method'] = df_restricted['method'].replace({'qiskit transpile': 'Transpile (Qiskit impl.)',
+#                                                                'ours': 'tableau (ours)',
+#                                                                'ours_temp': 'tableau w/ heat (ours)',
+#                                                                'ours_random': 'tableau w/ random pivot (ours)',
+#                                                                'Bravyi et al. (qiskit)': 'Bravyi et al. (Qiskit impl.)',
+#                                                                'Ewout van den Berg (stim)': 'Van den Berg (Stim impl.)'
+#                                                                })
+#     df_restricted = df_restricted[df_restricted["method"]
+#                                   != "tableau w/ heat (ours)"]
+#     df_restricted = df_restricted[df_restricted["method"]
+#                                   != "tableau w/ random pivot (ours)"]
 
-#     sns.lineplot(df, x="n_gadgets", y="s", hue="method")
-#     plt.title("S-Gates")
-#     plt.xlabel("Number of input gates")
-#     plt.ylabel("Number of S-Gates")
-#     plt.savefig(f"./{name}_s.pdf")
-#     plt.show()
+#     df_complete['method'] = df_complete['method'].replace({'qiskit transpile': 'Transpile (Qiskit impl.)',
+#                                                            'ours': 'tableau (ours)',
+#                                                            'ours_temp': 'tableau w/ heat (ours)',
+#                                                            'ours_random': 'tableau w/ random pivot (ours)',
+#                                                            'Bravyi et al. (qiskit)': 'Bravyi et al. (Qiskit impl.)',
+#                                                            'Ewout van den Berg (stim)': 'Van den Berg (Stim impl.)'
+#                                                            })
+#     df_complete = df_complete[df_complete["method"]
+#                               != "tableau w/ heat (ours)"]
+#     df_complete = df_complete[df_complete["method"]
+#                               != "tableau w/ random pivot (ours)"]
 
-#     sns.lineplot(df, x="n_gadgets", y="cx", hue="method")
-#     plt.title("CNOT-Gates")
-#     plt.xlabel("Number of input Gates")
-#     plt.ylabel("Number of CNOT-Gates")
-#     # add a vertical line for v_line_cx if not none
-#     if v_line_cx is not None:
-#         plt.axhline(y=v_line_cx, color="black", linestyle="--")
-#     plt.savefig(f"./{name}_cx.pdf")
-#     plt.show()
+    # fig, (ax1, ax2) = plt.subplots(
+    #     1, 2, sharex=True, figsize=(9, 4))
+    # gfg = sns.lineplot(df_restricted, x="n_gadgets", y="h", hue="method",
+    #                    ax=ax1)
+    # gfg.set_xlabel("Number of input Gates")
+    # gfg.set_ylabel("Number of H Gates")
+
+    # gfg = sns.lineplot(df_complete, x="n_gadgets", y="h", hue="method", ax=ax2)
+    # gfg.set_xlabel("Number of input Gates")
+    # gfg.set_ylabel("")
+    # ax1.get_legend().remove()
+    # ax2.get_legend().remove()
+    # handles, labels = ax1.get_legend_handles_labels()
+    # lgd = fig.legend(handles, labels,
+    #                  bbox_to_anchor=(0.95, 1.05), ncol=4, borderaxespad=1.)
+    # plt.savefig(f"./{name}_h.pdf", bbox_extra_artists=(lgd,),
+    #             bbox_inches='tight')
+    # plt.show()
+    # plt.clf()
+
+    # fig, (ax1, ax2) = plt.subplots(
+    #     1, 2, sharex=True, figsize=(9, 4))
+    # gfg = sns.lineplot(df_restricted, x="n_gadgets", y="s", hue="method",
+    #                    ax=ax1)
+    # gfg.set_xlabel("Number of input Gates")
+    # gfg.set_ylabel("Number of S Gates")
+
+    # gfg = sns.lineplot(df_complete, x="n_gadgets", y="s", hue="method", ax=ax2)
+    # gfg.set_xlabel("Number of input Gates")
+    # gfg.set_ylabel("")
+    # ax1.get_legend().remove()
+    # ax2.get_legend().remove()
+    # handles, labels = ax1.get_legend_handles_labels()
+    # lgd = fig.legend(handles, labels,
+    #                  bbox_to_anchor=(0.95, 1.05), ncol=4, borderaxespad=1.)
+    # plt.savefig(f"./{name}_s.pdf", bbox_extra_artists=(lgd,),
+    #             bbox_inches='tight')
+    # plt.show()
+    # plt.clf()
+
+    # fig, (ax1, ax2) = plt.subplots(
+    #     1, 2, sharex=True, figsize=(15, 5))
+    # plt.subplots_adjust(wspace=0.3)
+    # gfg = sns.lineplot(df_restricted, x="n_gadgets", y="cx", hue="method",
+    #                    ax=ax1)
+    # gfg.set_title("CNOT-Gates")
+    # gfg.set_xlabel("Number of input Gates")
+    # gfg.set_ylabel("Number of CNOT Gates")
+    # if v_line_cx is not None:
+    #     gfg.axhline(y=v_line_cx, color="black", linestyle="--")
+
+    # gfg = sns.lineplot(df_complete, x="n_gadgets",
+    #                    y="cx", hue="method", ax=ax2)
+    # gfg.set_title("CNOT-Gates")
+    # gfg.set_xlabel("Number of input Gates")
+    # gfg.set_ylabel("Number of CNOT Gates")
+    # ax1.get_legend().remove()
+    # ax2.get_legend().remove()
+    # if v_line_cx is not None:
+    #     gfg.axhline(y=v_line_cx, color="black", linestyle="--")
+    # handles, labels = ax1.get_legend_handles_labels()
+    # lgd = fig.legend(handles, labels, ncol=3, borderaxespad=1.)
+    # # plt.savefig(f"./{name}_cx.pdf", bbox_extra_artists=(lgd,),
+    # #             bbox_inches='tight')
+    # export_legend(lgd)
+    # plt.show()
+    # plt.clf()
+    # fig, (ax1, ax2) = plt.subplots(ncols=2)
+    # sns.lineplot(df_restricted, x="n_gadgets", y="s", hue="method", ax=ax1)
+    # sns.lineplot(df_complete, x="n_gadgets", y="s", hue="method", ax=ax2)
+    # plt.title("S-Gates")
+    # plt.xlabel("Number of input gates")
+    # plt.ylabel("Number of S-Gates")
+    # plt.savefig(f"./{name}_s.pdf")
+    # plt.show()
+    # plt.clf()
+
+    # fig, (ax1, ax2) = plt.subplots(ncols=2)
+    # sns.lineplot(df_restricted, x="n_gadgets", y="cx", hue="method", ax=ax1)
+    # sns.lineplot(df_complete, x="n_gadgets", y="cx", hue="method", ax=ax2)
+    # plt.title("CNOT-Gates")
+    # plt.xlabel("Number of input Gates")
+    # plt.ylabel("Number of CNOT-Gates")
+    # # add a vertical line for v_line_cx if not none
+    # if v_line_cx is not None:
+    #     plt.axhline(y=v_line_cx, color="black", linestyle="--")
+    # plt.savefig(f".//{name}_cx.pdf")
+    # plt.show()
+    # plt.clf()
+
+    # fig, (ax1, ax2) = plt.subplots(ncols=2)
+    # sns.lineplot(df_restricted, x="n_gadgets", y="depth", hue="method", ax=ax1)
+    # sns.lineplot(df_complete, x="n_gadgets", y="depth", hue="method", ax=ax2)
+    # plt.title("Circuit depth")
+    # plt.xlabel("Number of input Gates")
+    # plt.ylabel("Circuit Depth")
+    # # add a vertical line for v_line_cx if not none
+    # # if v_line_cx is not None:
+    # #     plt.axhline(y=v_line_cx, color="black", linestyle="--")
+    # plt.savefig(f".//{name}_depth.pdf")
+    # plt.show()
+    # plt.clf()
+
+    # fig, (ax1, ax2) = plt.subplots(ncols=2)
+    # sns.lineplot(df_restricted, x="n_gadgets",
+    #              y="2q_depth", hue="method", ax=ax1)
+    # sns.lineplot(df_complete, x="n_gadgets",
+    #              y="2q_depth", hue="method", ax=ax2)
+    # plt.title("2 Qubit Gate depth")
+    # plt.xlabel("Number of input Gates")
+    # plt.ylabel("2 Qubit Gate Depth")
+    # # add a vertical line for v_line_cx if not none
+    # if v_line_depth is not None:
+    #     plt.axhline(y=v_line_depth, color="black", linestyle="--")
+    # plt.savefig(f".//{name}_2qdepth.pdf")
+    # plt.show()
+    # plt.clf()
 
 
 def estimate_routing_overhead(arch_name, n_qubits, conv_gadgets):
@@ -135,14 +368,14 @@ def estimate_routing_overhead(arch_name, n_qubits, conv_gadgets):
 
 
 if __name__ == "__main__":
-    # df = pd.read_csv(f"./random_complete_5.csv")
-    # df_ = df[df["method"] == "Bravyi et al. (qiskit)"]
-    # df_ = df_[df_["n_gadgets"] > 75]
-    # v_line = np.mean(df_["cx"])
-    # v_line_depth = np.mean(df_["2q_depth"])
+    df = pd.read_csv(f"./random_complete_5.csv")
+    df_ = df[df["method"] == "Bravyi et al. (qiskit)"]
+    df_ = df_[df_["n_gadgets"] > 75]
+    v_line = np.mean(df_["cx"])
+    v_line_depth = np.mean(df_["2q_depth"])
 
-    # plot_experiment(name="random_complete_5", v_line_cx=v_line,
-    #                 v_line_depth=v_line_depth)
+    plot_experiment(name="random_complete_5", v_line_cx=v_line,
+                    v_line_depth=v_line_depth)
     # plot_experiment(name="random_quito", v_line_cx=v_line,
     #                 v_line_depth=v_line_depth)
 
@@ -202,12 +435,14 @@ if __name__ == "__main__":
     # df_ = df_[df_["n_gadgets"] > 500]
     # v_line_depth = np.mean(df_["2q_depth"])
 
-    # plot_experiment(name="random_brisbane", v_line_cx=v_line, v_line_depth=v_line_depth)
-    # plot_experiment(name="random_complete_127", v_line_cx=v_line, v_line_depth=v_line_depth)
+    # plot_experiment(name="random_brisbane", v_line_cx=v_line,
+    #                 v_line_depth=v_line_depth)
+    # plot_experiment(name="random_complete_127",
+    #                 v_line_cx=v_line, v_line_depth=v_line_depth)
 
     estimate_routing_overhead("quito", 5, 75)
     estimate_routing_overhead("nairobi", 7, 110)
     estimate_routing_overhead("guadalupe", 16, 250)
     estimate_routing_overhead("mumbai", 27, 500)
     estimate_routing_overhead("ithaca", 65, 1250)
-    # estimate_routing_overhead("brisbane", 127, 3000)
+    estimate_routing_overhead("brisbane", 127, 3000)
